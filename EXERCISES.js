8390345c645672c9ae0aa6de6837d8c315ab9018
Kiril Vandzsurak <kiril.wandzurak@gmail.com>
2022-10-05 14:50:13 +0200

    Exercise 9 completing

--- EXERCISES.js
+++ EXERCISES.js
@@ -127,4 +127,10 @@ if (x === 10) {
  try to search it by yourself, tomorrow morning we'll discuss it together!)
 */
 
-/* WRITE YOUR ANSWER HERE */
+/* let loto = 12;
+let victory = "VICTORY";
+let loser = "LOOSER";
+
+let result = loto === 12 ? victory : loser;
+
+console.log("The result of your loto is:", result); */
---
+++ excercise_9.js
@@ -0,0 +1,7 @@
+let loto = 12;
+let victory = "VICTORY";
+let loser = "LOOSER";
+
+let result = loto === 12 ? victory : loser;
+
+console.log("The result of your loto is:", result);
