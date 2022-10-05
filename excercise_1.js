b0339c7eeb4ae212d13e6f4a3f6d34ad70466d10
Kiril Vandzsurak <kiril.wandzurak@gmail.com>
2022-10-05 14:02:55 +0200

    Excercise 1 completing

---
+++ excercise_1.js
@@ -0,0 +1,14 @@
+console.log("\nIn JavaScript we have several datatypes:\n");
+let int = 100;
+let float = 15.5;
+let string = "Spectacled Caiman";
+let boolean = true;
+console.log("INT:", int);
+console.log("FLOAT:", float);
+console.log("STRING", string);
+console.log("BOOLEAN:", boolean);
+
+console.log(
+  "\nAnd the most interesting thing is that they are declaring with the same keyword 'let'"
+);
+console.log("This is because, JS language is considered to be loosely typed");
