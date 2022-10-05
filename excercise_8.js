5e6f31db4c68f229e7eee5035828a74d56430da9
Kiril Vandzsurak <kiril.wandzurak@gmail.com>
2022-10-05 17:30:57 +0200

    Update exercise 8 (else if)

--- excercise_8.js
+++ excercise_8.js
@@ -1,31 +1,22 @@
 let x = 9;
 if (x === 1) {
   console.log("one");
-}
-if (x === 2) {
+} else if (x === 2) {
   console.log("two");
-}
-if (x === 3) {
+} else if (x === 3) {
   console.log("three");
-}
-if (x === 4) {
+} else if (x === 4) {
   console.log("four");
-}
-if (x === 5) {
+} else if (x === 5) {
   console.log("five");
-}
-if (x === 6) {
+} else if (x === 6) {
   console.log("six");
-}
-if (x === 7) {
+} else if (x === 7) {
   console.log("seven");
-}
-if (x === 8) {
+} else if (x === 8) {
   console.log("eight");
-}
-if (x === 9) {
+} else if (x === 9) {
   console.log("nine");
-}
-if (x === 10) {
+} else if (x === 10) {
   console.log("ten");
 }
